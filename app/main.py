c822442d9e6861434fe813cad787c30abcee04ed
uke_mba <uke_sjtu@sjtu.edu.cn>
2025-12-02 15:12:39 +0800

    feat: implement executable files locating

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 
@@ -15,9 +16,26 @@ def main():
             continue
         if command.startswith("type "):
             _, cmd = command.split(" ", 1)
+            # Check cmd sequence:
+            # 1. Check if cmd is a builtin shell command
+            # 2. Go through every directory in PATH
+            # 3. Mark command as not found
             if cmd in ["echo", "exit", "type"]:
                 print(f"{cmd} is a shell builtin")
             else:
+                PATH = os.getenv("PATH")
+                for dir in PATH.split(os.pathsep):
+                    # 1. Check if a file with the command name exists.
+                    full_path = os.path.join(dir, cmd)
+                    if os.path.exists(full_path):
+                        # 2. Check if the file has execute permissions.
+                        if os.access(full_path, os.X_OK):
+                            # 3. If the file exists and has execute permissions, print <command> is <full_path> and stop.
+                            print(f"{cmd} is {full_path}")
+                            break
+                        else:
+                            # 4. If the file exists but lacks execute permissions, skip it and continue to the next directory.
+                            continue
                 print(f"{cmd}: not found")
             continue
         print(f"{command}: command not found")
