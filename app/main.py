50a6eb6649f21374d29dd639fb31a499338afb40
uke_mba <uke_sjtu@sjtu.edu.cn>
2025-12-02 14:50:00 +0800

    feat: pass stage 3

--- app/main.py
+++ app/main.py
@@ -2,12 +2,12 @@ import sys
 
 
 def main():
-    sys.stdout.write("$ ")
+    while True:
+        sys.stdout.write("$ ")
 
-    # Wait for user input
-    command = input()
-    print(f"{command}: command not found")
-    pass
+        # Wait for user input
+        command = input()
+        print(f"{command}: command not found")
 
 
 if __name__ == "__main__":
